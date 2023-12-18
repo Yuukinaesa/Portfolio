83133a47786b5819287f5b85ff6baa616871f427
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2023-12-18 11:09:37 +0700

    Update index.js

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,13 +1,7 @@
-import nextjs from "./img/projects/nextjs.jpg";
-import reactjs from "./img/projects/reactjs.jpg";
-import laravel from "./img/projects/laravel.jpg";
-import codeigniter from "./img/projects/codeigniter.jpg";
-import vuejs from "./img/projects/vuejs.jpg";
-import nuxtjs from "./img/projects/nuxtjs.jpg";
 import analisisjakarta from "./img/projects/analisisjakarta.png";
 import webanalisisjakarta from "./img/projects/webanalisisjakarta.png";
 import landmarkjupyter from "./img/projects/landmarkjupyter.png";
 import landmarkstreamlit from "./img/projects/landmarkstreamlit.png";
 import rocus from "./img/projects/rocus.png";
 import cybersec from "./img/projects/cybersec.jpg";
-export { nextjs, reactjs, laravel, codeigniter, vuejs, nuxtjs, analisisjakarta, webanalisisjakarta, landmarkjupyter, landmarkstreamlit, rocus, cybersec };
+export { analisisjakarta, webanalisisjakarta, landmarkjupyter, landmarkstreamlit, rocus, cybersec };
