2b818b7bd76d1542572d7f05db7892bcda2a6468
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2023-12-19 15:14:44 +0700

    Update Experience.jsx
    update

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const ExperienceItem = ({ name, url, position, date }) => (
-  <div className="flex justify-between lg:px-10 mb-6">
-    <div className="flex flex-col">
-      <a href={url} target="_blank" className="text-base font-medium text-primary tracking-wide hover:text-sky">
-        {name}
-      </a>
-      <span className="text-base text-semidark">{position}</span>
+const ExperienceItem = ({ name, url, position, date, foundationUrl }) => {
+  const [mainName, subName] = name.split(" (");
+
+  return (
+    <div className="flex justify-between lg:px-10 mb-6">
+      <div className="flex flex-col">
+        <a
+          href={url}
+          target="_blank"
+          className="text-base font-medium text-primary tracking-wide hover:text-sky"
+        >
+          {mainName}
+        </a>
+        {subName && (
+          <span>
+            (
+            <a href={foundationUrl || url} target="_blank" className="text-primary tracking-wide hover:text-sky">
+              {subName.replace(")", "")}
+            </a>
+            )
+          </span>
+        )}
+        <span className="text-base text-semidark">{position}</span>
+      </div>
+      <div className="flex flex-col text-right mt-1">
+        <span className="text-sm text-semidark">{date}</span>
+      </div>
     </div>
-    <div className="flex flex-col text-right mt-1">
-      <span className="text-sm text-semidark">{date}</span>
-    </div>
-  </div>
-);
+  );
+};
 
 const Experience = () => {
   return (
@@ -28,31 +45,32 @@ const Experience = () => {
       </h3>
       <div className="grid grid-cols-1 lg:ml-96">
         <ExperienceItem
-          name="GoTo Impact Foundation"
-          url="https://www.goto-impact.org/"
+          name="PT GoTo Gojek Tokopedia Tbk (GoTo Impact Foundation)"
+          url="https://www.gotocompany.com/"
+          foundationUrl="https://www.goto-impact.org/"
           position="Data Analyst"
           date="Jul 2023 - Dec 2023"
         />
         <ExperienceItem
-          name="Carrot Academy"
+          name="PT Karoten Asia Pasifik (Carrot Academy)"
           url="https://www.carrotacademy.com/"
           position="Data Entry"
           date="Dec 2022 - Jan 2023"
         />
         <ExperienceItem
-          name="Ministry of Communication and Informatics"
+          name="Kementerian Komunikasi dan Informatika RI (KOMINFO)"
           url="https://www.kominfo.go.id/"
           position="Cyber Security Analyst"
           date="Aug 2022 - Dec 2022"
         />
         <ExperienceItem
-          name="Telkom Indonesia"
+          name="PT Telekomunikasi Indonesia Tbk (Telkom Indonesia)"
           url="https://www.telkom.co.id/"
           position="Customer Service Representative"
           date="Jan 2017 - Feb 2017"
         />
         <ExperienceItem
-          name="Xiaomi Technology"
+          name="Xiaomi Corporation"
           url="http://en.miui.com/forum.php"
           position="MIUI Beta Team"
           date="Jan 2016 - Dec 2016"
